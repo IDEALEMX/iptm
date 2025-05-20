4476babc06de1f905638c5240f26e5ec93e25a38
IDEALEMX <lnxp5acnt@gmail.com>
2025-05-19 20:20:11 -0600

    added command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    // handle program arguments
+    let args: Vec<String> = env::args().collect();
+
+    if let Some(first_arg) = args.get(1) {
+        match first_arg.as_str() {
+            "hello" => println!("Hello world!"),
+            _ => eprintln!("Error, unrecognized arguments"),
+        }
+    } else {
+        eprintln!("Error, missing arguments");
+    }
 }
+
